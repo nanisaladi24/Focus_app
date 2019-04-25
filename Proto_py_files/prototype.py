660b9484c1f8cd6a8e0dd1f59a30cc6593d2ab5c
nanisaladi24 <nanisaladi24@gmail.com>
2019-04-25 00:21:40 -0700

    first commit of project

---
+++ Proto_py_files/prototype.py
@@ -0,0 +1,7 @@
+"""
+Trying to implement a proto type of desired application.
+This file just contains algorithmic implementation of the project. Most likely everything might be in CMD I/O.
+Goal of this file is to implement core functionalities of application and be able to test them.
+"""
+
+print ("Trial code to commit")
